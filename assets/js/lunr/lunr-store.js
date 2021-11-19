e1e9a7f35a4f349ae16c057a0fb0b016314e61b3
Guillermo <alu0101520377@ull.edu.es>
2021-11-19 15:06:21 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,67 @@
+var store = [{
+        "title": "Biblia",
+        "excerpt":" Deja que los muertos entierren a sus muertos; tu vete a anunciar el Reino de Dios  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/biblia",
+        "teaser": null
+      },{
+        "title": "Tranvia",
+        "excerpt":" ejercicio LATEX   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\]  ejercicio DATA                                First name                Last name                Age                Location                               John            Doe            35            United States                              Jane            Doe            29            France                              Jack            Hill            25            Australia              ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/tranvia",
+        "teaser": null
+      },{
+        "title": "Comer",
+        "excerpt":"albondigas  ","categories": [],
+        "tags": [],
+        "url": "/comer",
+        "teaser": null
+      },{
+        "title": "Guillermo",
+        "excerpt":"me llamo Guillermo Van Hoye  ","categories": [],
+        "tags": [],
+        "url": "/Guillermo",
+        "teaser": null
+      },{
+        "title": "Fuga",
+        "excerpt":"La fuga de San Diego La fuga de San Diego de Alcalá, fuga de San Diego o más popularmente Día de la fuga, es una tradición muy arraigada en las Islas Canarias mi clase somos 20 alumnos Hoy es jueves La respuesta es 4 Me gusta mucho comer narajas Me...","categories": [],
+        "tags": [],
+        "url": "/fuga",
+        "teaser": null
+      },{
+        "title": "Generador",
+        "excerpt":"#Aprendiendo a usar un Generador Estático  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/generador",
+        "teaser": null
+      },{
+        "title": "Belgian",
+        "excerpt":"   Helaasheid der dingen   Broken Circle Breakdown  ","categories": ["peliculas"],
+        "tags": [],
+        "url": "/peliculas/Belgian",
+        "teaser": null
+      },{
+        "title": "Classics",
+        "excerpt":"To kill a mockingbird  ","categories": ["peliculas"],
+        "tags": [],
+        "url": "/peliculas/Classics",
+        "teaser": null
+      },{
+        "title": "Spanish",
+        "excerpt":"   La lengua de las mariposas   Mientras dure la guerra  ","categories": ["peliculas"],
+        "tags": [],
+        "url": "/peliculas/Spanish",
+        "teaser": null
+      },{
+        "title": "Familias",
+        "excerpt":"Becky G. Familia entre asertivo (abuelos) y permisivo (padres). Los padres son muy jóvenes y los ve como hermanos es como si no tuviera padres de ahí que la familia es permisiva. Pero luego habla del abuelito que para haber sido capaz de transmitir algunos valores. Lourdes lleva la familia...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/familias",
+        "teaser": null
+      },{
+        "title": "Aprendizaje cooperativo",
+        "excerpt":"II. Grupos cooperativos Tipos de grupos: informales, formales y de base Tamaño de los grupos Depende del Tiempo, Experiencia previa, Edad y Material) Hay con elegir un tamaño en función de la estabilidad necesaria para que el grupo llega a ser eficaz en el tiempo disponible. Si hay 10 meses...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/aprendizaje-cooperativo",
+        "teaser": null
+      }]
